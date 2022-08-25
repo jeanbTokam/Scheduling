9dff69ce3b4bc46bc05eefb76e2be6391019cbc9
jb Tokam <jbtokam85@gmail.com>
2022-08-24 23:50:20 -0400

    set up MongoDB

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require('express')
 
 const app = express()
+require('dotenv').config()
 
 const port = process.env.PORT || 5000
 
+
+
+
+
 app.listen(port,() => console.log(`Node server started at port ${port}`))
+
+
+
